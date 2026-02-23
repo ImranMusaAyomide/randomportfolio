6b4090af0e51a29c081cf9a5b429fecaff46528d
ImranMusaAyomide <haajmuskid@gmail.com>
2026-02-23 17:38:52 +0100

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/Header'
 import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
 
 function App() {
   return (
     <>
       <Header />
       <HeroSection />
+      <FeatureSection />
     </>
   )
 }
