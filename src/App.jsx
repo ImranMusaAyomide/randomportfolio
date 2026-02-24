fbef94917134a34b670ed2f8c51f22ae4d78d038
ImranMusaAyomide <haajmuskid@gmail.com>
2026-02-24 17:41:08 +0100

    Doen latest creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import './App.css'
-import Header from './components/Header'
-import HeroSection from './components/HeroSection'
-import FeatureSection from './components/FeatureSection'
-import ImgGallary from './components/ImgGallary'
-import Launch from './components/Launch'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import FeatureSection from './components/FeatureSection';
+import ImgGallary from './components/ImgGallary';
+import Launch from './components/Launch';
+import LatestCreationPage from './components/LatestCreationPage';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <HeroSection />
-      <FeatureSection />
-      <ImgGallary />
-      <Launch />
-    </>
-  )
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <HeroSection />
+              <FeatureSection />
+              <ImgGallary />
+              <Launch />
+            </>
+          }
+        />
+        <Route path="/latest" element={<LatestCreationPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
